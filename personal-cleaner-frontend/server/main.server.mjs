559af84ef34387f9a3681788c42196638bf4ed8b
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2024-01-05 22:21:59 -0300

    chore

--- personal-cleaner-frontend/server/main.server.mjs
+++ personal-cleaner-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FH3OT2EX.mjs";import"./chunk-SS6BVVQW.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-CBFQQ6WF.mjs";import"./chunk-S3FADEBS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
