0da36f928325d16e9123366b4c39eab147bdc616
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2024-01-06 20:02:16 -0300

    CHORE

--- personal-cleaner-frontend/server/main.server.mjs
+++ personal-cleaner-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CBFQQ6WF.mjs";import"./chunk-S3FADEBS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-GSVF4HIS.mjs";import"./chunk-S3FADEBS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
